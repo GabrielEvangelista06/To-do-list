ee91200588db5287088eab5166d4fcb0268aea1d
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-04-27 21:40:41 -0300

    Corrigindo conexão com o banco de dados

--- config/database.js
+++ config/database.js
@@ -8,11 +8,11 @@ mongoose.Promise = global.Promise;
 mongoose
   .connect(process.env.MONGODB_HOST, {
     useNewUrlParser: true,
-    useUndefinedTopology: true,
   })
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
+    console.log('Deu erro');
     console.log(err);
   });

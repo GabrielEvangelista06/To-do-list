e51932b0271f74e923550fa99203c5d207aa1cfe
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-11 23:21:11 -0300

    Alterando o que as requsições nas rotas vão retornar, agora elas renderizam páginas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,10 +6,12 @@ const Checklist = require('../models/ChecklistModel');
 
 router.get('/', async (req, res) => {
   try {
-    let checklist = await Checklist.find({});
-    res.status(200).json(checklist);
+    let checklists = await Checklist.find({});
+    res.status(200).render('checklists/index', { checklists: checklists });
   } catch (err) {
-    res.status(500).json(err);
+    res
+      .status(500)
+      .render('pages/error', { error: 'Erro ao exibir as listas' });
   }
 });
 
@@ -27,9 +29,11 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(200).render('checklists/show', { checklist: checklist });
   } catch (err) {
-    res.status(422).json(err);
+    res
+      .status(422)
+      .render('pages/error', { error: 'Erro ao exibir as listas de tarefas' });
   }
 });
 

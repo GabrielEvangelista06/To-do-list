4f9817b364fa6df7d0b2a8594abb14a98565526d
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-11 22:27:45 -0300

    Criando rotas para acessar a página principal no frontend

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
-require('./config/database');
+const path = require('path');
 const checklistRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+require('./config/database');
 
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
 
 app.listen(port, () => {
